6ded26b2435472a3c5fca48ef3d7a755d27e8d49
Precious Usanga <precioususanga001@gmail.com>
2020-12-23 18:03:31 +0100

    update

--- src/app/admin/report/activity-report/activity-report.component.ts
+++ src/app/admin/report/activity-report/activity-report.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Observable, of } from 'rxjs';
 import { CrudService } from 'src/app/core/services/crud.service';
 import { environment } from 'src/environments/environment';
+import { saveAs } from 'file-saver';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-activity-report',
@@ -35,7 +38,9 @@ export class ActivityReportComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private crudService: CrudService
+    private crudService: CrudService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
   ) { }
 
   ngOnInit(): void {
@@ -70,19 +75,23 @@ export class ActivityReportComponent implements OnInit {
   }
 
   getActivityReport(filterBy?) {
+
     if (filterBy && filterBy.hasOwnProperty('period')) {
       this.getActivityReportByFrequency(filterBy);
     } else {
+      this.spinner.show();
       const activityReportUrl = `${this.getActivityReportUrl}/${this.employeeDetails.emp_id}`;
       const payload = filterBy || '';
       this.crudService.createData(activityReportUrl, payload).subscribe(
         data => {
           console.log(data);
           if (data.responseCode === '00') {
+            this.spinner.hide();
             this.activityReports = data.responseObject;
           }
         },
         error => {
+          this.spinner.hide();
           console.log(error);
         }
       );
@@ -90,15 +99,18 @@ export class ActivityReportComponent implements OnInit {
   }
 
   getActivityReportByFrequency(filter) {
+    this.spinner.show();
     const activityReportUrl = `${this.getActivityReportUrl}/${this.employeeDetails.emp_id}/${filter.period}`;
     this.crudService.getData(activityReportUrl).subscribe(
       data => {
         console.log(data);
         if (data.responseCode === '00') {
-          this.activityReports = data.responseObject;
+          this.spinner.hide();
+          this.activityReports = data.responseObject.activities;
         }
       },
       error => {
+        this.spinner.hide();
         console.log(error);
       }
     );
@@ -118,13 +130,77 @@ export class ActivityReportComponent implements OnInit {
     );
   }
 
-  downloadReport() {
-    console.log('download clicked');
-  }
-
   displayFilter(filterselection) {
     console.log(filterselection);
     this.filterOption = filterselection;
   }
 
+  convertToCSV(objArray) {
+    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let activityData = '';
+
+    for (let i = 0; i < array.length; i++) {
+      let activity = '';
+      for (const index in array[i]) {
+        if (index) {
+          if (activity !== '') {
+            activity += ',';
+          }
+          activity += array[i][index];
+        }
+      }
+      activityData += activity + '\r\n';
+    }
+    return activityData;
+  }
+
+  exportCSVFile(headers, items, fileTitle) {
+    if (headers) {
+        items.unshift(headers);
+    }
+
+    // Convert Object to JSON
+    const activityJSON = JSON.stringify(items);
+    // Create CSV File
+    const csv = this.convertToCSV(activityJSON);
+    const fileName = `${fileTitle}.csv`;
+
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    this.spinner.hide();
+    saveAs(blob, `${fileName}`);
+    this.toastr.success(`${fileName} downloaded`, 'Download successful');
+  }
+
+  downloadReport() {
+    this.spinner.show();
+    const headers = {
+      activity: 'Activity',
+      objective: 'Objective',
+      unit: 'Unit',
+      status: 'Status',
+      remarks: 'Remarks',
+      activityDate: 'Activity Date'.replace(/,/g, '') // remove commas to avoid errors
+    };
+
+    const activityReports = this.activityReports;
+
+    const formattedactivityReports = [];
+    // format the data
+    activityReports.forEach((activity) => {
+      const activityDate = new Date(activity.activity_date);
+      formattedactivityReports.push({
+        activity: `${activity.activity}`.replace(/,/g, ''), // remove commas to avoid errors
+        objective: `${activity.objectives}`,
+        unit: `${activity.units}`.replace(/,/g, ''),
+        status: `${activity.status}`,
+        remarks: `${activity.remarks}`.replace(/,/g, ''),
+        activityDate: `${activityDate.toString()}`.slice(0, 15),
+      });
+    });
+
+    const fileTitle = `Activity_Report`; // or 'my-unique-title'
+    console.log(`activityReports: ${activityReports}, fileTitle: ${fileTitle}`);
+    // call the exportCSVFile() function to process the JSON and trigger the download
+    this.exportCSVFile(headers, formattedactivityReports, fileTitle);
+  }
 }
--- src/app/admin/report/leave-report/leave-report.component.ts
+++ src/app/admin/report/leave-report/leave-report.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CrudService } from 'src/app/core/services/crud.service';
 import { environment } from 'src/environments/environment';
+import { saveAs } from 'file-saver';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-leave-report',
@@ -14,10 +17,14 @@ export class LeaveReportComponent implements OnInit {
   employeeDetails = JSON.parse(localStorage.getItem('user'));
   getLeaveUrl = environment.reportsUrl.leaveReport;
   leaveReports: any;
+  payloadRange;
 
   constructor(
     private fb: FormBuilder,
-    private crudService: CrudService) { }
+    private crudService: CrudService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    ) { }
 
   ngOnInit(): void {
     this.initForm();
@@ -37,27 +44,99 @@ export class LeaveReportComponent implements OnInit {
 
   onSubmit(formPayload) {
     const payload = {
-      startDate: formPayload.startDate.value,
-      endDate: formPayload.endDate.value
+      start_date: formPayload.startDate.value,
+      end_date: formPayload.endDate.value
     };
+    this.payloadRange = payload;
     console.log(payload);
     this.getLeaveReport(payload);
   }
 
   getLeaveReport(filterBy?) {
+    this.spinner.show();
     const leaveReportUrl = `${this.getLeaveUrl}/${this.employeeDetails.emp_id}`;
     const payload = filterBy || '';
     this.crudService.createData(leaveReportUrl, payload).subscribe(
       data => {
         console.log(data);
         if (data.responseCode === '00') {
+          this.spinner.hide();
           this.leaveReports = data.responseObject;
         }
       },
       error => {
+        this.spinner.hide();
         console.log(error);
       }
     );
+  }
+
+  convertToCSV(objArray) {
+    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let leaveData = '';
+
+    for (let i = 0; i < array.length; i++) {
+      let leave = '';
+      for (const index in array[i]) {
+        if (index) {
+          if (leave !== '') {
+            leave += ',';
+          }
+          leave += array[i][index];
+        }
+      }
+      leaveData += leave + '\r\n';
+    }
+    return leaveData;
+  }
+
+  exportCSVFile(headers, items, fileTitle) {
+    if (headers) {
+        items.unshift(headers);
+    }
+
+    // Convert Object to JSON
+    const leaveJSON = JSON.stringify(items);
+    // Create CSV File
+    const csv = this.convertToCSV(leaveJSON);
+    const fileName = `${fileTitle}.csv`;
+
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    this.spinner.hide();
+    saveAs(blob, `${fileName}`);
+    this.toastr.success(`${fileName} downloaded`, 'Download successful');
+  }
+
+  downloadReport() {
+    this.spinner.show();
+    const headers = {
+      leaveNo: 'Leave No'.replace(/,/g, ''),
+      leaveType: 'Leave Type'.replace(/,/g, ''),
+      leaveDaysLeft: 'Leave Days Left'.replace(/,/g, ''),
+      leaveStart: 'Leave Start'.replace(/,/g, ''),
+      leaveEnd: ''.replace(/,/g, '') // remove commas to avoid errors
+    };
+
+    const leaveReports = this.leaveReports;
+
+    const formattedLeaveReports = [];
+    // format the data
+    leaveReports.forEach((leave) => {
+      const leaveStart = new Date(leave.start_date);
+      const leaveEnd = new Date(leave.end_date);
+      formattedLeaveReports.push({
+        leaveNo: `${leave.leave_no}`,
+        leaveType: `${leave.leave_type}`.replace(/,/g, ''), // remove commas to avoid errors
+        leaveDaysLeft: `${leave.leave_left}`,
+        leaveStart: `${leaveStart.toString()}`.slice(0, 15),
+        leaveEnd: `${leaveEnd.toString()}`.slice(0, 15)
+      });
+    });
+
+    const fileTitle = `leave_Report_from_${this.payloadRange.start_date}_to_${this.payloadRange.end_date}`; // or 'my-unique-title'
+    console.log(`leaveReports: ${leaveReports}, fileTitle: ${fileTitle}`);
+    // call the exportCSVFile() function to process the JSON and trigger the download
+    this.exportCSVFile(headers, formattedLeaveReports, fileTitle);
   }
 
 }
