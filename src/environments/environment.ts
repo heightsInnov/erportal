4306450b2298aea75cbe461f6df9a77fdb3d799a
Jinadu Ayotola O <ayo02034reel@yahoo.com>
2021-06-08 22:02:15 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://6f4de6004510.ngrok.io/erportal/',
+  apiBaseUrl: 'https://f387d16bd27a.ngrok.io/erportal/',
   loginUrl: 'secured/user/login',
   resetPasswordUrl: 'secured/user/password-reset',
   employeeUrl: {
